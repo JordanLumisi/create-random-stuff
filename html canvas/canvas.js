828240057d60a603cb791bd4e8ac1608f59932d1
Jordan Clark <jordan@lumisi.com>
2025-04-02 19:05:46 +0100

    feat(html-canvas): experimenting with FA swirls

--- html canvas/canvas.js
+++ html canvas/canvas.js
@@ -1,11 +1,18 @@
+const pageWidth = window.innerWidth;
+
 function draw() {
 	const canvas = document.getElementById("canvas");
 
 	if (canvas.getContext) {
 		const ctx = canvas.getContext("2d");
+		ctx.canvas.width = pageWidth;
+		ctx.canvas.height = pageWidth;
 
-		for (let i = 0; i < 10; i++) {
-			roundedRect(ctx, 100, 100, 400, 400, 20, 4);
+		for (let i = 0; i < 60; i++) {
+			// roundedRect(ctx, 400, 400, 800, 800, 20, 4);
+			roundedRect(ctx, 100, 100, pageWidth, pageWidth, 20, 4);
+			// roundedRect(ctx, 400, 400, 40, 40, 400, 16);
+			// roundedRect(ctx, 400, 400, pageWidth, pageWidth, 400, 16);
 			i++;
 		}
 	}
@@ -14,9 +21,28 @@ function draw() {
 window.addEventListener("load", draw);
 
 function roundedRect(ctx, x, y, width, height, radius, rotate) {
-	const faGrad = ctx.createLinearGradient(x, y, x + width, y + height);
-	faGrad.addColorStop(0, "#F15623");
-	faGrad.addColorStop(1, "#AD1C27");
+	const faGrad = ctx.createLinearGradient(x, y + height, x + width, y);
+	faGrad.addColorStop(0, "rgb(0, 0, 29, 0)"); // slate
+	faGrad.addColorStop(0.33, "rgb(0, 0, 54, 0)"); // indigo
+	faGrad.addColorStop(0.66, "#AD1C27"); // red
+	faGrad.addColorStop(1, "#F15623"); // orange
+
+	// Experimental gradient
+	// faGrad.addColorStop(0, "#AD1C27"); // red
+	// faGrad.addColorStop(0.33, "#F15623"); // orange
+	// faGrad.addColorStop(0.66, "rgb(0, 0, 29, 0)"); // slate
+	// faGrad.addColorStop(1, "rgb(0, 0, 54, 0)"); // indigo
+
+	// Experimental gradient
+	// faGrad.addColorStop(0, "#F15623"); // orange
+	// faGrad.addColorStop(0.5, "rgb(0, 0, 29, 0)"); // slate
+	// faGrad.addColorStop(0.5, "rgb(0, 0, 54, 0)"); // indigo
+	// faGrad.addColorStop(1, "#AD1C27"); // red
+
+	// faGrad.addColorStop(0, "#AD1C27"); // red
+	// faGrad.addColorStop(1, "#F15623"); // orange
+
+	ctx.lineWidth = 10;
 
 	ctx.strokeStyle = faGrad;
 	ctx.save();
@@ -28,7 +54,8 @@ function roundedRect(ctx, x, y, width, height, radius, rotate) {
 	ctx.arcTo(x, y, x, y + radius, radius);
 	ctx.stroke();
 	ctx.translate(width / 2 + x, width / 2 + x); // translate to rectangle center
-	ctx.rotate((Math.PI / 180) * rotate);
+	ctx.rotate((Math.PI / 180) * -rotate);
+	ctx.scale(0.94, 0.94); // scale down
 	ctx.translate(-(width / 2 + x), -(width / 2 + x)); // translate back
 	// ctx.restore();
 }
