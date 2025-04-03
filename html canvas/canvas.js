080f00a730fc85ffb236258a3953ceb5a78024b8
Jordan Clark <jordan@lumisi.com>
2025-04-03 13:19:55 +0100

    feat(canvas-html): added range/number input

--- html canvas/canvas.js
+++ html canvas/canvas.js
@@ -1,20 +1,28 @@
 const pageWidth = window.innerWidth;
+const iterations = document.querySelector("#iterations");
+const canvas = document.getElementById("canvas");
 
 function draw() {
-	const canvas = document.getElementById("canvas");
-
 	if (canvas.getContext) {
 		const ctx = canvas.getContext("2d");
 		ctx.canvas.width = pageWidth;
 		ctx.canvas.height = pageWidth;
 
-		for (let i = 0; i < 60; i++) {
-			roundedRect(ctx, pageWidth / 4, pageWidth / 4, 800, 800, 20, 4);
-			// roundedRect(ctx, 100, 100, pageWidth, pageWidth, 20, 4);
-			// roundedRect(ctx, 400, 400, 40, 40, 400, 16);
-			// roundedRect(ctx, 400, 400, pageWidth, pageWidth, 400, 16);
-			i++;
-		}
+		iterations.addEventListener("input", event => {
+			let iterationValue = parseInt(event.target.value, 10);
+			iterations.textContent = iterationValue;
+
+			// Clear the canvas before redrawing
+			ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+			// Loop with updated iteration value
+			for (let i = 0; i < iterationValue; i++) {
+				roundedRect(ctx, pageWidth * 0.3, pageWidth * 0.3, 800, 800, 20, 4);
+				// roundedRect(ctx, 100, 100, pageWidth, pageWidth, 20, 4);
+				// roundedRect(ctx, 400, 400, 40, 40, 400, 16);
+				// roundedRect(ctx, 400, 400, pageWidth, pageWidth, 400, 16);
+			}
+		});
 	}
 }
 
@@ -22,10 +30,10 @@ window.addEventListener("load", draw);
 
 function roundedRect(ctx, x, y, width, height, radius, rotate) {
 	const faGrad = ctx.createLinearGradient(x, y + height, x + width, y);
-	faGrad.addColorStop(0, "rgb(0, 0, 29, 0)"); // slate
-	faGrad.addColorStop(0.33, "rgb(0, 0, 54, 0)"); // indigo
-	faGrad.addColorStop(0.66, "#AD1C27"); // red
-	faGrad.addColorStop(1, "#F15623"); // orange
+	// faGrad.addColorStop(0, "rgb(0, 0, 29, 0)"); // slate
+	// faGrad.addColorStop(0.33, "rgb(0, 0, 54, 0)"); // indigo
+	// faGrad.addColorStop(0.66, "#AD1C27"); // red
+	// faGrad.addColorStop(1, "#F15623"); // orange
 
 	// Experimental gradient
 	// faGrad.addColorStop(0, "#AD1C27"); // red
@@ -39,8 +47,8 @@ function roundedRect(ctx, x, y, width, height, radius, rotate) {
 	// faGrad.addColorStop(0.5, "rgb(0, 0, 54, 0)"); // indigo
 	// faGrad.addColorStop(1, "#AD1C27"); // red
 
-	// faGrad.addColorStop(0, "#AD1C27"); // red
-	// faGrad.addColorStop(1, "#F15623"); // orange
+	faGrad.addColorStop(0, "#AD1C27"); // red
+	faGrad.addColorStop(1, "#F15623"); // orange
 
 	ctx.lineWidth = 10;
 
