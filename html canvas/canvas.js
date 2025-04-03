d5d1bd72be754b6a81e7ffeba9b510275fdcd309
Jordan Clark <jordan@lumisi.com>
2025-04-03 16:44:02 +0100

    exp(html-canvas): playing with FA spirals

--- html canvas/canvas.js
+++ html canvas/canvas.js
@@ -1,6 +1,9 @@
 const pageWidth = window.innerWidth;
-const iterations = document.querySelector("#iterations");
+const pageHeight = window.outerHeight;
+const iterationSlider = document.querySelector("#iterations");
+const iterationValue = document.querySelector("#iteration-value");
 const canvas = document.getElementById("canvas");
+const canvasWidth = 800;
 
 function draw() {
 	if (canvas.getContext) {
@@ -8,32 +11,43 @@ function draw() {
 		ctx.canvas.width = pageWidth;
 		ctx.canvas.height = pageWidth;
 
-		iterations.addEventListener("input", event => {
-			let iterationValue = parseInt(event.target.value, 10);
-			iterations.textContent = iterationValue;
+		// Initial draw with default value
+		drawWithIterations(ctx, parseInt(iterationSlider.value, 10));
 
-			// Clear the canvas before redrawing
-			ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-
-			// Loop with updated iteration value
-			for (let i = 0; i < iterationValue; i++) {
-				roundedRect(ctx, pageWidth * 0.3, pageWidth * 0.3, 800, 800, 20, 4);
-				// roundedRect(ctx, 100, 100, pageWidth, pageWidth, 20, 4);
-				// roundedRect(ctx, 400, 400, 40, 40, 400, 16);
-				// roundedRect(ctx, 400, 400, pageWidth, pageWidth, 400, 16);
-			}
+		// Update on slider change
+		iterationSlider.addEventListener("input", event => {
+			const value = parseInt(event.target.value, 10);
+			iterationValue.textContent = value;
+			drawWithIterations(ctx, value);
 		});
 	}
 }
 
-window.addEventListener("load", draw);
+function drawWithIterations(ctx, iterationValue) {
+	// Clear the canvas before redrawing
+	// ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+	// Loop with iteration value
+	for (let i = 0; i < iterationValue; i++) {
+		// roundedRect(ctx, 0, 0, pageWidth, pageWidth, 120, pageWidth / 100);
+		roundedRect(
+			ctx,
+			pageWidth / 2 - canvasWidth / 2, // x
+			600, // y
+			canvasWidth, // width
+			canvasWidth, // height
+			60,
+			4
+		);
+	}
+}
 
 function roundedRect(ctx, x, y, width, height, radius, rotate) {
 	const faGrad = ctx.createLinearGradient(x, y + height, x + width, y);
-	// faGrad.addColorStop(0, "rgb(0, 0, 29, 0)"); // slate
-	// faGrad.addColorStop(0.33, "rgb(0, 0, 54, 0)"); // indigo
-	// faGrad.addColorStop(0.66, "#AD1C27"); // red
-	// faGrad.addColorStop(1, "#F15623"); // orange
+	faGrad.addColorStop(0, "rgb(0, 0, 29, 0)"); // slate
+	faGrad.addColorStop(0.33, "rgb(0, 0, 54, 0)"); // indigo
+	faGrad.addColorStop(0.66, "#AD1C27"); // red
+	faGrad.addColorStop(1, "#F15623"); // orange
 
 	// Experimental gradient
 	// faGrad.addColorStop(0, "#AD1C27"); // red
@@ -43,13 +57,18 @@ function roundedRect(ctx, x, y, width, height, radius, rotate) {
 
 	// Experimental gradient
 	// faGrad.addColorStop(0, "#F15623"); // orange
-	// faGrad.addColorStop(0.5, "rgb(0, 0, 29, 0)"); // slate
-	// faGrad.addColorStop(0.5, "rgb(0, 0, 54, 0)"); // indigo
+	// faGrad.addColorStop(0.33, "rgb(0, 0, 29, 0)"); // slate
+	// faGrad.addColorStop(0.66, "rgb(0, 0, 54, 0)"); // indigo
 	// faGrad.addColorStop(1, "#AD1C27"); // red
 
-	faGrad.addColorStop(0, "#AD1C27"); // red
-	faGrad.addColorStop(1, "#F15623"); // orange
+	// faGrad.addColorStop(0, "rgb(0, 0, 54, 0)"); // indigo
+	// faGrad.addColorStop(0.75, "#AD1C27"); // red
+	// faGrad.addColorStop(1, "#F15623"); // orange
 
+	// faGrad.addColorStop(0, "#AD1C27"); // red
+	// faGrad.addColorStop(1, "#F15623"); // orange
+
+	// ctx.lineWidth = pageWidth / 100;
 	ctx.lineWidth = 10;
 
 	ctx.strokeStyle = faGrad;
@@ -67,3 +86,5 @@ function roundedRect(ctx, x, y, width, height, radius, rotate) {
 	ctx.translate(-(width / 2 + x), -(width / 2 + x)); // translate back
 	// ctx.restore();
 }
+
+window.addEventListener("load", draw);
